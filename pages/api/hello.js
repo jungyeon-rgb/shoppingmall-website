1cf69aa99e2971dc41691ebb01dca011cfa43539
[IoT]김정연 <jy.keem2468@gmail.com>
2023-02-16 11:21:17 +0000

    [ADD]index.jsx, [id].jsx 파일 생성

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: '정연님 한나님 ㅎㅇ' });
+  res.status(200).json({ name: "정연님 한나님 ㅎㅇ", title: "테스트입니당" });
 }
---
+++ pages/products/index.jsx
@@ -0,0 +1,7 @@
+export default function Products() {
+  return (
+    <div>
+      <h2>제품페이지입니다람쥐</h2>
+    </div>
+  );
+}
