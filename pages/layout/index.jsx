d5e8898dfe0e4b58837aa484525caea5814e2e49
김정연 <jy.keem2468@gmail.com>
2023-02-20 17:37:03 +0900

    [FEAT]main-navigation파일 생성 및 구현 시도

---
+++ pages/layout/index.jsx
@@ -0,0 +1,14 @@
+import MainNavigation from "./main-navigation";
+
+function Layout() {
+  return (
+    <div>
+      <MainNavigation />
+      <main>
+        <h1>Nav 테스트중입니다</h1>
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
