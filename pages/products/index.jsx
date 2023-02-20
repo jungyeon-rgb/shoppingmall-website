fabb2b41e92ebe7fe687d2c0007e1be12d038dfe
김정연 <jy.keem2468@gmail.com>
2023-02-20 17:58:45 +0900

    [CHORE]products폴더의 index.jsx 수정

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,7 +1,25 @@
-export default function Products() {
+import Image from "next/image";
+import Link from "next/link";
+
+function ProductItem(props) {
+  const { title, image, price } = props.post;
+
+  const imagePath = `./images/products/${image}`;
   return (
-    <div>
-      <h2>제품페이지입니다람쥐</h2>
-    </div>
+    <li className={classes.post}>
+      <Link>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={300} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <p>${price}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
   );
 }
+
+export default ProductItem;
