2a4c869df34586bd765bfaad76f8ea2964112c2c
김정연 <jy.keem2468@gmail.com>
2023-02-20 20:40:00 +0900

    products error

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,21 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
-import sytles from "./products/product-item.module.css";
+import styles from "./productItem.modules.css";
 
 function ProductItem(props) {
   const { title, image, price } = props.post;
 
   const imagePath = `./images/products/${image}`;
   return (
-    <li className={classes.post}>
+    <li className={styles.post}>
       <Link>
         <a>
-          <div className={classes.image}>
+          <div className={styles.image}>
             <Image src={imagePath} alt={title} width={300} height={300} />
           </div>
-          <div className={classes.content}>
+          <div className={styles.content}>
             <h3>{title}</h3>
-            <p>${price}</p>
+            <p>{price}</p>
           </div>
         </a>
       </Link>
