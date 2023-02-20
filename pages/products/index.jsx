3839bef7df68883606ce65df8bccf626ea933c3c
김정연 <jy.keem2468@gmail.com>
2023-02-20 21:20:36 +0900

    fix

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,33 +1,18 @@
-<<<<<<< HEAD
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./productItem.modules.css";
-=======
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './productItem.module.css';
->>>>>>> 8171babb3a1b3efd1c1f647aec1d6fc25f52edd8
+import styles from "./productItem.module.css";
 
 export default function Products(props) {
   const { title, image, price } = props.post;
 
   return (
     <li className={styles.post}>
-<<<<<<< HEAD
-      <Link>
-        <a>
-          <div className={styles.image}>
-            <Image src={imagePath} alt={title} width={300} height={300} />
-          </div>
-          <div className={styles.content}>
-=======
-      <Link href={'/'}>
+      <Link href={"/"}>
         <div>
           <div>
             <Image src={image} alt={title} width={300} height={300} />
           </div>
           <div>
->>>>>>> 8171babb3a1b3efd1c1f647aec1d6fc25f52edd8
             <h3>{title}</h3>
             <p>{price}</p>
           </div>
@@ -41,9 +26,9 @@ export async function getServerSideProps() {
   // const res = await fetch('http://localhost:3000/api/products');
   // const posts = await res.json();
   const post = {
-    title: 'test',
-    image: 'https://picsum.photos/500',
-    price:  1000,
+    title: "test",
+    image: "https://picsum.photos/500",
+    price: 1000,
   };
 
   return { props: { post } };
