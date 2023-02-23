670e3b32e6afca41997bfebf811b1fb636a66c8c
김정연 <jy.keem2468@gmail.com>
2023-02-23 14:49:05 +0900

    [FEAT]component 폴더  내에 productdetail 관련 파일생성 및 코드작성

---
+++ components/Product/product-detail/product-header.jsx
@@ -0,0 +1,15 @@
+/* 선택한 게시물의 메인 제목이나 이미지를 포함하는
+ 메인 헤더 데이터가 들어갈 파일까지 생성 */
+import Image from "next/image";
+import classes from "./product-content.styles";
+
+export default function ProductHeader(props) {
+  const { id, maker, slug, title, content, price, image } = props;
+
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Image scr={image} alt={title} width={200} height={150} />
+    </header>
+  );
+}
