37c5522f5d12e53929c2c5181c7f02a153523352
김정연 <jy.keem2468@gmail.com>
2023-02-28 10:49:11 +0900

    [CHORE]주문파트 코드 작성

--- pages/api/products/[id]/index.js
+++ pages/api/products/[id]/index.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-    const productId = req.query.id;
-    res.status(200).json({ id: productId, title: '상품명', price: 1000 });
+  const productId = req.query.id;
+  res.status(200).json({ id: productId, title: "상품명", price: 1000 });
 }
